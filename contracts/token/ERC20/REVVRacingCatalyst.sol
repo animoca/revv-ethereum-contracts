758d6adecefb3d5e5f4f9cba202adc8364369600
Lucas Tseng <lucast@animocabrands.com>
2023-10-27 17:25:01 +0800

    Readd REVVMotorsportShardClaim and remove all other deployed contracts

--- contracts/token/ERC20/REVVRacingCatalyst.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-import {IForwarderRegistry} from "@animoca/ethereum-contracts/contracts/metatx/interfaces/IForwarderRegistry.sol";
-import {ERC20MintBurn} from "./ERC20MintBurn.sol";
-
-contract REVVRacingCatalyst is ERC20MintBurn {
-    constructor(IForwarderRegistry forwarderRegistry) ERC20MintBurn("REVV Racing Catalyst", "CATA", 18, forwarderRegistry) {}
-}
---
+++ test/contracts/payment/REVVMotorsportShardClaim.test.js
@@ -0,0 +1,67 @@
+const {ethers} = require('hardhat');
+const {expect} = require('chai');
+const {constants} = ethers;
+const {MerkleTree} = require('merkletreejs');
+const keccak256 = require('keccak256');
+const {deployContract} = require('@animoca/ethereum-contract-helpers/src/test/deploy');
+const {loadFixture} = require('@animoca/ethereum-contract-helpers/src/test/fixtures');
+const {getForwarderRegistryAddress} = require('@animoca/ethereum-contracts/test/helpers/registries');
+
+describe('REVVMotorsportShardClaim', function () {
+  let other;
+
+  before(async function () {
+    [deployer, claimer1, claimer2, claimer3, claimer4, other] = await ethers.getSigners();
+  });
+
+  const fixture = async function () {
+    const forwarderRegistryAddress = await getForwarderRegistryAddress();
+    this.shards = await deployContract('ERC20MintBurn', '', '', 18, forwarderRegistryAddress);   
+    this.contract = await deployContract('REVVMotorsportShardClaim', this.shards.address);
+    await this.shards.grantRole(await this.shards.MINTER_ROLE(), this.contract.address);
+  };
+
+  beforeEach(async function () {
+    await loadFixture(fixture, this);
+  });
+
+  context('claimPayout(address,bytes,bytes32[])', function () {
+    beforeEach(async function () {
+      this.nextNonce = (await this.contract.nonce()).add(constants.One);
+
+      this.elements = [
+        {
+          claimer: claimer1.address,
+          amount: 1,
+        },
+        {
+          claimer: claimer2.address,
+          amount: 2,
+        },
+        {
+          claimer: claimer3.address,
+          amount: 3,
+        },
+        {
+          claimer: claimer4.address,
+          amount: 4,
+        },
+      ];
+      this.leaves = this.elements.map((el) =>
+        ethers.utils.solidityPack(
+          ['address', 'bytes', 'uint256'],
+          [el.claimer, ethers.utils.defaultAbiCoder.encode(['uint256'], [el.amount]), this.nextNonce]
+        )
+      );
+      this.tree = new MerkleTree(this.leaves, keccak256, {hashLeaves: true, sortPairs: true});
+      this.root = this.tree.getHexRoot();
+      await this.contract.setMerkleRoot(this.root);
+    });
+    it('mints the SHRD', async function () {
+      const claimData = ethers.utils.defaultAbiCoder.encode(['uint256'], [this.elements[0].amount]);
+      await expect(this.contract.claimPayout(this.elements[0].claimer, claimData, this.tree.getHexProof(keccak256(this.leaves[0]))))
+        .to.emit(this.shards, 'Transfer')
+        .withArgs(constants.AddressZero, this.elements[0].claimer, this.elements[0].amount);
+    });
+  });
+});
